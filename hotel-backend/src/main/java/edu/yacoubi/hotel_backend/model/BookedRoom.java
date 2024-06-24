c82dad3d2e50bf16c3a712fb78c3784a9d5796ec
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-06-25 01:20:50 +0200

    JPA Room & BookedRoom entities

--- hotel-backend/src/main/java/edu/yacoubi/hotel_backend/model/BookedRoom.java
+++ hotel-backend/src/main/java/edu/yacoubi/hotel_backend/model/BookedRoom.java
@@ -1,4 +1,70 @@
 package edu.yacoubi.hotel_backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long bookingId;
+
+    @Column(name = "check_In")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_Out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_FullName")
+    private String guestFullName;
+
+    @Column(name = "guest_Email")
+    private String guestEmail;
+
+    @Column(name = "n_adults")
+    private int numberOfAdults;
+
+    @Column(name = "n_children")
+    private int numberOfChildren;
+
+    @Column(name = "total_guest")
+    private int totalNumberOfGuest;
+
+    @Column(name = "confirmation_code")
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumberOfGuest() {
+        this.totalNumberOfGuest = this.numberOfAdults + this.numberOfChildren;
+    }
+
+    public void setNumberOfAdults(int numberOfAdults) {
+        this.numberOfAdults = numberOfAdults;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setNumberOfChildren(int numberOfChildren) {
+        this.numberOfChildren = numberOfChildren;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+        calculateTotalNumberOfGuest();
+    }
 }
--- hotel-backend/src/main/java/edu/yacoubi/hotel_backend/model/Room.java
+++ hotel-backend/src/main/java/edu/yacoubi/hotel_backend/model/Room.java
@@ -1,10 +1,44 @@
 package edu.yacoubi.hotel_backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Room {
     @Id
-    private int id;
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked = false;
+    @Lob
+    private Blob photo;
+
+    @OneToMany(fetch = LAZY, cascade = ALL)
+    private List<BookedRoom> bookings = new ArrayList<>(); // historical
+
+    public void addBooking(BookedRoom booking) {
+        bookings.add(booking);
+        booking.setRoom(this);
+        this.isBooked = true;
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        booking.setBookingConfirmationCode(bookingCode);
+    }
 }
