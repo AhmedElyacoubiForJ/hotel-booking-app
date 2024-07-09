13ff99348aaacf7fb9503ecfcaa128f51ef108f8
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-07-09 18:20:08 +0200

    navbar

--- hotel-frondend/src/App.jsx
+++ hotel-frondend/src/App.jsx
@@ -1,8 +1,9 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Footer from "./components/layout/Footer"
+import Footer from "./components/layout/Footer";
+import NavBar from "./components/layout/NavBar";
 import RoomList from "./components/room/RoomList";
 import Home from "./components/home/Home";
 import EditRoom from "./components/room/EditRoom";
@@ -10,18 +11,18 @@ import AddRoom from "./components/room/AddRoom";
 function App() {
   return (
     <>
-    <main>
-      <Router>
-        <Routes>
-          {/* <Route exact path="/" element={<Home />} /> */}
-          <Route exact path="/" element={<RoomList />} />
-          <Route exact path="/edit-room/:roomId" element={<EditRoom />} />
-          <Route exact path="/rooms-list" element={<RoomList />} />
-          <Route exact path="/add-room" element={<AddRoom />} />
-        </Routes>
+      <main>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/add-room" element={<AddRoom />} />
+            <Route exact path="/rooms-list" element={<RoomList />} />
+            <Route exact path="/edit-room/:roomId" element={<EditRoom />} />
+          </Routes>
+        </Router>
         <Footer />
-      </Router>
-    </main>
+      </main>
     </>
   );
 }
--- hotel-frondend/src/components/layout/NavBar.jsx
+++ hotel-frondend/src/components/layout/NavBar.jsx
@@ -1,11 +1,80 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      NavBar
-    </div>
-  )
-}
+    <nav className="navbar navbar-expand-lg bg-body-tertiary px-5 shadow mt-5 sticky-top">
+      <div className="container-fluid">
+        <Link to={"/"} className="navbar-brand">
+          <span className="hotel-color">Anonym Hotel</span>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav navbar-nav-scroll me-auto my-2 my-lg-0">
+            <li className="nav-item">
+              <NavLink
+                to={"/browse-all-rooms"}
+                className="nav-link"
+                aria-current="page"
+              >
+                Browse all rooms
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to={"/admin"} className="nav-link" aria-current="page">
+                Admin
+              </NavLink>
+            </li>
+          </ul>
+          <ul className="navbar-nav d-flex">
+            <li className="nav-item">
+              <NavLink className="nav-link" to={"/find-booking"}>
+                Find My Booking
+              </NavLink>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className={`nav-link dropdown-toggle`}
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Account
+              </a>
+              <ul className="dropdown-menu show" aria-labelledby="navbarDropdown">
+                <li className="dropdown-item">
+                  <Link to={"/login"} className="dropdown-item">
+                    Login
+                  </Link>
+                </li>
+                <li className="dropdown-item">
+                  <Link to={"/profile"} className="dropdown-item">
+                    Profile
+                  </Link>
+                </li>
+                <li className="dropdown-item">
+                  <Link to={"/logout"} className="dropdown-item">
+                    Logout
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
