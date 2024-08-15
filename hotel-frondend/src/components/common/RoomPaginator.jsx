251cb9713c5103665eafaf30b2b580314941d77b
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-15 12:11:50 +0200

    added comments

--- hotel-frondend/src/components/common/RoomPaginator.jsx
+++ hotel-frondend/src/components/common/RoomPaginator.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+// RoomPaginator component that displays pagination for rooms
 const RoomPaginator = ({ totalPages, currentPage, handleGoToPage }) => {
+  // Creating an array of page numbers from 1 to totalPages
   const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
   return (
     <nav>
       <ul className="pagination justify-content-center mt-2">
         {pageNumbers.map((pageNumber) => (
+          // Each page number is rendered as a list item
           <li
             key={pageNumber}
             className={`page-item ${
